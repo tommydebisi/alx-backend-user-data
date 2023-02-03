ac50b339ca1d97225b1653a4225ba304b75de55f
tommydebisi <tommydebisi@gmail.com>
2023-02-03 08:06:53 +0100

    :seedling: use of ? regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,19 +6,11 @@ from typing import List
 import re
 
 
-def getPattern(message: str, pattern: str, separator: str) -> str:
-    """ gets pattern needed to substitute """
-    spli_list = message.split(separator)
-    for item in spli_list:
-        if pattern in item:
-            return item[item.index('=') + 1:]
-        return ''
-
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ returns the log message obfuscated """
     for field in fields:
-        pat = getPattern(message, field, separator)
-        message = re.sub(pat, redaction, message)
+        regex = r'{}=.*?{}'.format(field, separator)
+        repl_str = '{}={}{}'.format(field, redaction, separator)
+        message = re.sub(regex, repl_str, message)
     return message
