f1cddd6f6ebb2ccf9c531546249d9e839cc43350
tommydebisi <tommydebisi@gmail.com>
2023-02-16 03:57:13 +0100

    :seedling: validated the login details

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
     auth module
 """
 from db import DB, User
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -33,4 +33,16 @@ class Auth:
                 raise ValueError(f"User {email} already exists")
         except (InvalidRequestError, NoResultFound):
             hashed_pass = _hash_password(password)
-            return self._db.add_user(email, hashed_pass)
+            return self._db.add_user(email, hashed_pass.decode('utf-8'))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            Checks login details and returns True if info provided
+            is correct else it returns False
+        """
+        try:
+            usr_inst = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'),
+                           usr_inst.hashed_password.encode('utf-8'))
+        except (InvalidRequestError, NoResultFound):
+            return False
