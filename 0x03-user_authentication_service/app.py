50e29e0319e0bafe1f0ed0aef870f5763936acf9
tommydebisi <tommydebisi@gmail.com>
2023-02-16 04:33:01 +0100

    :seedling: login route configured

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
     App module
 """
-from flask import Flask, jsonify, request as rq
+from flask import Flask, jsonify, request as rq, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -29,6 +29,22 @@ def reg_user():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+        Login users
+    """
+    email, passwd = rq.form.get('email'), rq.form.get('password')
+
+    if not auth.valid_login(email, passwd):
+        abort(401)
+
+    ses_id = auth.create_session(email)
+    payload = jsonify({"email": email, "message": "logged in"})
+    payload.set_cookie('session_id', ses_id)
+    return payload
 
 
 if __name__ == "__main__":
